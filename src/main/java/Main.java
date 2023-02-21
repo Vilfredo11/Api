0a1dbb437cf52074729869d79086bcc520eef3d2
Vilfredo <tardileo1996@gmail.com>
2023-02-21 22:58:19 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Michael";
+        post.passport = "6666 № 66666666";
+        post.patronymic = "Petrovich";
+        post.phone = "911";
+        post.surname = "Zubenko";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = "11";
+        post.birthday.month = "2";
+        post.birthday.year = "1996";
+
+
+    }
+}
